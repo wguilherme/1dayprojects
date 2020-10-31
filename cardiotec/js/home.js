477254b84bf6b120f2386b88017efb2be3846da9
W. Guilherme <ravok123@gmail.com>
2020-10-31 16:30:47 -0300

    convênios

---
+++ cardiotec/js/home.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper('.swiper-container', {
+   slidesPerView: 6,
+   spaceBetween: 30,
+   // centeredSlides: true,
+   pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+   },
+
+   navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+
+});
