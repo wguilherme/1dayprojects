5f3dd6fda127f64728eb3f0a6e67b55784eaf6a8
W. Guilherme <ravok123@gmail.com>
2020-11-02 19:33:27 -0300

    [Update] Responsive layout

--- cardiotec/js/home.js
+++ cardiotec/js/home.js
@@ -1,5 +1,5 @@
 var swiper = new Swiper('.swiper-container', {
-   slidesPerView: 5,
+   slidesPerView: 3,
    spaceBetween: 30,
    // centeredSlides: true,
    pagination: {
@@ -11,6 +11,17 @@ var swiper = new Swiper('.swiper-container', {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
+
+
+   //  responsive
+   breakpoints: {
+      650: {
+         slidesPerView: 4
+      },
+      900: {
+         slidesPerView: 5
+      }
+   }
 
 
 });
