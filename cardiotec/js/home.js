9d9171b7219dbf0c50cc84c38aa1125761c79691
W. Guilherme <ravok123@gmail.com>
2020-11-07 11:33:42 -0300

    update: layout

--- cardiotec/js/home.js
+++ cardiotec/js/home.js
@@ -1,27 +1,14 @@
 var swiper = new Swiper('.swiper-container', {
-   slidesPerView: 3,
-   spaceBetween: 30,
-   // centeredSlides: true,
-   pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
+  slidesPerView: 5,
+  spaceBetween: 30,
+  // centeredSlides: true,
+  pagination: {
+     el: '.swiper-pagination',
+     clickable: true,
+  },
+
+  navigation: {
+     nextEl: '.swiper-button-next',
+     prevEl: '.swiper-button-prev',
    },
-
-   navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-
-
-   //  responsive
-   breakpoints: {
-      650: {
-         slidesPerView: 4
-      },
-      900: {
-         slidesPerView: 5
-      }
-   }
-
-
 });
